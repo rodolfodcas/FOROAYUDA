61debe628a0ca6f71ddf43ab6dcee6ecd7ba1d58
Rodolfo DCAS <rodeck_dc@hotmail.com>
2021-05-18 22:39:56 -0500

    v1.0

---
+++ js/reports.js
@@ -0,0 +1,13 @@
+new Typed('.reportes', {
+    strings: [
+        "UBER",
+        "DIDI",
+        "SECUESTROS",
+        "ACOSO",
+        "VIOLENCIA",
+        "DISCRIMINACIÓN",
+        "RACISMO, ETC."
+    ],
+    typeSpeed: 100,
+    loop: true,
+});
